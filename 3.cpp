da797a7b18d0ca65734241e49cdc5687c1939ce9
Nadeem Gulam <68492875+NadeemGulam@users.noreply.github.com>
2021-09-30 17:49:59 +0530

    3

---
+++ 3.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void pattern(int n ){
+    for (int  i =1; i <= n; i++)
+    {
+        for (int  j = 0; j <=n-i; j++)
+        {
+            cout<<"* ";
+        
+        }
+        cout<<endl;  
+    }
+    
+}
+int main(){
+    int n=5;
+    pattern(n);
+    return 0;
+}
